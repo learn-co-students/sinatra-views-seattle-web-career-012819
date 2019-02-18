19c71d0731e7a4b5c91312f76d5178cb5b28f0b7
github username <github email address>
2019-02-17 21:19:32 -0800

    First commit. Added HTML to info.

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		
-	end
+  # Getting used to writing method calls instead of defining methods.
+  # Standardizing muscle memory in OTBS in prep for learning JS later on.
+
+  get ('/') {
+    erb :index
+  }
+
+  get ('/info') {
+    erb :info
+  }
 end
